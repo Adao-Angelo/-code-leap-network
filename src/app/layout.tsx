d2c8b3d2d92c4bebae7d6dfed660ebb2d624b131
Adao-Angelo <adaobegginer@gmail.com>
2026-03-10 11:42:01 +0100

    feat: initial setup with Next.js, Tailwind, and Roboto font

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+import { Roboto } from 'next/font/google';
+import './globals.css';
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700', '900'],
+  variable: '--font-roboto',
+});
+
+export const metadata: Metadata = {
+  title: 'CodeLeap Frontend Test',
+  description: 'Frontend technical test built with Next.js',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={`${roboto.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
